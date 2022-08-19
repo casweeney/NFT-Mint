e725e8f9638f5846e5d7082ee676724689812746
CodingCas <casweeno2000@gmail.com>
2022-08-19 19:47:39 +0100

    added all links

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,9 @@
 import { ethers } from 'hardhat';
 
+/// Opensea image URL: https://testnets.opensea.io/assets/rinkeby/0xb326edbc49aadba2d63b2e69564f8f9757e50f95/0
+/// Contract Address on Rinkeby:  0xb326EDBc49aaDBA2d63B2E69564f8F9757e50F95
+/// Deployment Transaction Hash: 0xc844dd005ef801948b739f387e524bc2baa48787eae2b475c94e72a3afdda8c1
+
 async function main() {
   const CasNFT = await ethers.getContractFactory('CasNFT');
   const nft = await CasNFT.deploy();
@@ -9,8 +13,6 @@ async function main() {
   console.log('NFT deployed to:', nft.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
